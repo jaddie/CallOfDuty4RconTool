96ac89f7ad29b2e52a9cfca45c33c7e9832f0201
Jamez <James@Jaddie.co.uk>
2010-09-28 22:49:10 +0100

    Add initial Functional version.
    Contains start of schedule system reader.
    Lots more work to be done, first example function version.
    Current Dev Rule:Code First,Make it work however, refactor / optimize later.

---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Cod4RconConsoleTool
+{
+    class Program
+    {
+        public static IPAddress IP;
+        public static int Port;
+        public static string RconPassword;
+        public static string RconCommand;
+        public static Socket Client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (IP != null && Port != 0 && RconPassword != null && RconCommand != null)
+                {
+                    Console.WriteLine("Would you like to use your previous information? - Please enter yes or no");
+                    if (Console.ReadLine().ToLower() == "yes")
+                    {
+                        if (!string.IsNullOrEmpty(RconCommand))
+                        {
+                            Console.WriteLine("Would you like to run the same command again? - yes or no");
+                            if(Console.ReadLine().ToLower() == "yes")
+                            {
+                                Console.WriteLine("Executing previous command...");
+                                Console.WriteLine(Rcon(IP, Port, RconPassword, RconCommand));
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Enter command to execute");
+                                RconCommand = Console.ReadLine();
+                                Console.WriteLine(Rcon(IP, Port, RconPassword, RconCommand));
+                                return;
+                            }
+
+                        }
+                    }
+                }
+                    Console.WriteLine("Enter IP");
+                    IP = IPAddress.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Port");
+                    Port = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter RCon Password");
+                    RconPassword = Console.ReadLine();
+                    Console.WriteLine("Enter RCon Command To Run");
+                    RconCommand = Console.ReadLine();
+                    Console.WriteLine("Connecting..");
+                    Console.WriteLine(Rcon(IP, Port, RconPassword, RconCommand));
+                    Console.WriteLine("Command Executed, you are now at an rcon terminal\n(in english you can now just type commands to be sent directly do not put rcon at the start)\nIt will be added automaticlly for you.\nTo close the program type exitrcon");
+                    var input = "";
+                    while (input.ToLower() != "exitrcon")
+                    {
+                        input = Console.ReadLine();
+                        Console.WriteLine(Rcon(IP, Port, RconPassword, input));
+                    }
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Error value was empty, please try again!\nPress enter to start again...");
+                Console.ReadLine();
+                Main(null);
+            }
+            finally
+            {
+                Console.WriteLine("Program finished,press enter to exit or enter r to start from the beginning again");
+                var reply = Console.ReadLine();
+                if (reply.ToLower() == "r")
+                {
+                    Main(null);
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+        public static string Rcon(IPAddress gameServerIP, int gameServerPort, string password, string rconCommand)
+        {
+
+
+            // Connect to the server
+            if(!Client.Connected)
+            Client.Connect(gameServerIP, gameServerPort);
+
+
+            string command = "rcon " + password + " " + rconCommand;
+            Byte[] bufferTemp = Encoding.ASCII.GetBytes(command);
+            Byte[] bufferSend = new Byte[bufferTemp.Length + 5];
+
+
+            // Send the standard 5 characters before the messageessage, prevents disconnect
+            bufferSend[0] = Byte.Parse("255");
+            bufferSend[1] = Byte.Parse("255");
+            bufferSend[2] = Byte.Parse("255");
+            bufferSend[3] = Byte.Parse("255");
+            bufferSend[4] = Byte.Parse("02");
+
+            int j = 5;
+
+            for (int i = 0; i < bufferTemp.Length; i++)
+            {
+                bufferSend[j] = bufferTemp[i];
+                j++;
+            }
+
+
+            //IPEndPoint remoteIpEndPoint IPEndPoint(IPAddress.Any, 0);
+            Client.Send(bufferSend, SocketFlags.None);
+
+            // Use a large recieve buffer to make sure we can take the response
+            Byte[] bufferRec = new Byte[64999];
+            Client.Receive(bufferRec);
+            String result = Encoding.ASCII.GetString(bufferRec).Replace("\0", ""); // Remove whitespace
+            return result;
+
+        }
+    }
+}
---
+++ ScheduleHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Cod4RconConsoleTool
+{
+    class ScheduleHandler
+    {
+        public static List<string> ReadScheduleFile(string file = "schedule.config")
+        {
+            if (File.Exists(file))
+            {
+                var filelines = new List<string>();
+                var schedulefile = new StreamReader(file);
+                while (!schedulefile.EndOfStream)
+                {
+                    filelines.Add(schedulefile.ReadLine());
+                }
+                return filelines;
+            }
+            else
+            {
+                File.Create("schedule.config");
+                throw new FileNotFoundException(
+                    "Error schedule.config file is missing! -- New file generated, please edit and try again");
+            }
+        }
+    }
+}
